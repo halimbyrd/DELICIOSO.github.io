3e4f177aef285c27bef2f76750ad61b42cf46220
Halim <abdulhalimibnbyrd@gmail.com>
2023-03-04 18:49:44 -0500

    finished 'team' section

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const mobileMenu = document.querySelector('.mobile-nav-wrapper');
 const iconX = document.querySelector('.fa-x');
+
+const menuItems = document.querySelectorAll('.menu-item');
+const allItemsBtn = document.querySelector('.all-items-btn');
+const lunchBtn = document.querySelector('.lunch-btn');
+const dinnerBtn = document.querySelector('.dinner-btn');
+const dessertBtn = document.querySelector('.dessert-btn');
+const beverageBtn = document.querySelector('.beverage-btn');
+const menuSection = document.querySelector('.menu-section');
 const menuCategories = document.querySelectorAll('.menu-category');
 
 
-
+console.log(beverageBtn);
 
 
 
@@ -22,11 +30,78 @@ iconX.addEventListener('click', function() {
 })
 
 
-menuCategories.forEach(function(menuCategory) {
-    menuCategory.addEventListener('click', function() {
-        console.log('hello world')
+
+
+
+
+
+
+function showAllItems() {
+    menuItems.forEach(function(item) {
+        item.style.display = 'flex';
+    }) 
+};
+
+
+
+
+function showLunchItems() {
+      menuItems.forEach(function(item) {
+         if (item.classList.contains('lunch')) {
+            item.style.display = 'flex';
+            menuSection.style.padding = '1rem'
+
+         } else {
+            item.style.display = 'none';
+         }
     })
-})
+};
+
+
+
+function showDinnerItems() {
+    menuItems.forEach(function(item) {
+       if (item.classList.contains('dinner')) {
+          item.style.display = 'flex';
+          menuSection.style.padding = '1rem'
+
+       } else {
+          item.style.display = 'none';
+       }
+  })
+};
+
+
+
+
+
+function showDessertItems() {
+    menuItems.forEach(function(item) {
+       if (item.classList.contains('dessert')) {
+          item.style.display = 'flex';
+          menuSection.style.padding = '1rem'
+
+       } else {
+          item.style.display = 'none';
+       }
+  })
+};
+
+
+
+
+
+function showBeverageItems() {
+    menuItems.forEach(function(item) {
+       if (item.classList.contains('beverage')) {
+          item.style.display = 'flex';
+          menuSection.style.padding = '1rem'
+
+       } else {
+          item.style.display = 'none';
+       }
+  })
+};
 
 
 
@@ -36,6 +111,12 @@ menuCategories.forEach(function(menuCategory) {
 
 
 
+allItemsBtn.addEventListener('click', showAllItems);
 
+lunchBtn.addEventListener('click', showLunchItems);
 
+dinnerBtn.addEventListener('click', showDinnerItems);
 
+dessertBtn.addEventListener('click', showDessertItems)
+
+beverageBtn.addEventListener('click', showBeverageItems);
